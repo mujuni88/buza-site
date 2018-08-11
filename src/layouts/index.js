b0a86c7d6cc517cabfb571b0d844da3e1e082296
Joe Buza <joebm08@gmail.com>
2018-08-11 16:44:16 -0500

    add name and links

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
 import Header from '../components/header'
 import './index.css'
 
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
@@ -14,17 +23,10 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    {/*<Header siteTitle={data.site.siteMetadata.title} />*/}
+    <Wrapper>
       {children()}
-    </div>
+    </Wrapper>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,54 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+`
+
+const Name = styled.div`
+  font-family: Madelyn;
+  font-size: 10em;
+`
+
+const SocialMediaWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+`
+
+const SocialMediaLink = styled.a`
+  font-size: 16px;
+`
+
+const links = [
+  {
+    icon: (color) => <FaGithub color={color} />,
+    label: 'Github',
+    color: 'black',
+    url: 'https://github.com/mujuni88'
+  },
+  {
+    icon: (color) => <FaLinkedin color={color} />,
+    label: 'Linkedln',
+    color: '#0077b5',
+    url: 'https://linkedin.com/in/joebuza/'
+  }
+]
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  <Container>
+    <Name>Joe Buza.</Name>
+    <SocialMediaWrapper>
+      {links.map(({icon, label, color, url}) => 
+          <SocialMediaLink href={url} key={label}>
+            {icon(color)}
+          </SocialMediaLink>
+        )}
+    </SocialMediaWrapper>
+  </Container>
 )
 
 export default IndexPage
